c7b803482c24f2016081e325e90699045434cac5
mustafa2smi-ui <mustafa2smi@gmail.com>
2025-09-20 21:42:43 +0530

    Create posts.js

---
+++ posts.js
@@ -0,0 +1,15 @@
+const allPosts = [
+    {
+        title: "5 Amazing Places to Visit in India",
+        summary: "Discover five breathtaking travel destinations you must see.",
+        image: "assets/images/post-image-1.jpg",
+        link: "posts/post1.html"
+    },
+    // Naya post yahan add karein
+    {
+        title: "Top 10 Mobile Photography Tricks",
+        summary: "Learn how to take professional-looking photos with just your phone.",
+        image: "assets/images/post-image-2.jpg",
+        link: "posts/post2.html"
+    }
+];
